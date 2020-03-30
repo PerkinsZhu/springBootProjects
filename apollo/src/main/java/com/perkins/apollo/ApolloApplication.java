cd7d435d2aab6ea9a02dbf00e631a4992b01f94d
Perkins Zhu <perkinszhu@sina.cn>
2020-03-30 18:30:26 +0800

    some data

---
+++ apollo/src/main/java/com/perkins/apollo/ApolloApplication.java
@@ -0,0 +1,16 @@
+package com.perkins.apollo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApolloApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApolloApplication.class, args);
+    }
+
+}
