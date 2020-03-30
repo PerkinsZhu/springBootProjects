e330551d58039d15ab3c6c75e29b7d4949003278
Perkins Zhu <perkinszhu@sina.cn>
2020-03-30 18:26:37 +0800

    apollo test

---
+++ apollo/src/main/java/com/perkins/apollo/controller/BaseController.java
@@ -0,0 +1,32 @@
+package com.perkins.apollo.controller;
+
+import com.ctrip.framework.apollo.util.http.HttpResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping(value = "/test")
+public class BaseController {
+    @Value("${test.name}")
+    String name;
+    @Value("${test.age}")
+    String age;
+    @Value("${local.age}")
+    String localAge;
+
+
+    @GetMapping(value = "/name")
+    @ResponseBody
+    public String getName(HttpServletRequest request) {
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(localAge);
+        return "sss";
+    }
+}
